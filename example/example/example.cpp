b9eedf066928020cffd2986331d00548f7783217
Wiktor321 <wiktortomaszek@gmail.com>
2021-12-10 21:46:59 +0100

    Adds an empty C++ project
    Empty Code::Blocks project

---
+++ example/example/example.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+
+	int n, suma;
+	cout << "Podaj n: ";
+	cin >> n;
+	suma = 0;
+
+	for (int i = 2; i <= n; i += 2)
+	{
+		suma += i;
+	}
+
+	cout << "Suma parzystych elementow ciagu " << n << "> jest rowna 0 " << suma;
+
+
+	return 0;
+}
+
+
